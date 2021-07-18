e2e566142f74201ea7f7f98c46950b02c3f69dac
gulddaggi <81425764+gulddaggi@users.noreply.github.com>
2021-07-18 22:19:42 +0900

    Animal

---
+++ Prototype2/Assets/Script/DestroyOutOtBounds.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOtBounds : MonoBehaviour
+{
+    private float topBound = 30;
+    private float lowerBound = -10;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (transform.position.z > topBound)
+        {
+            Destroy(gameObject);
+        }
+        else if (transform.position.z < lowerBound)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
